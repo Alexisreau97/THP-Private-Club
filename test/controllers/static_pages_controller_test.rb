dc6a7c2f70ab27f49f3a2a1358c04c2e048e8f6c
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-05 18:24:27 +0100

    add first test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
+  test 'link home page' do
+    get '/'
+    if @current_user
+    	assert_select 'a', "Page du club"
+    else
+    	assert_select 'a', "Connexion"
+    	assert_select 'a', "S'incrire"
+    end
+  end
 end
