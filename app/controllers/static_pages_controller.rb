f091c4441eee1e419339741216516b392dd87c1e
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-05 15:30:48 +0100

    add page inderdiction

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
+  def index
+  	
+  end
+
   def club
-  	@user = User.all
+  	if logged_in?
+  		@user = User.all
+  	else
+  		flash[:danger] = "Bien essayé petit malin mais le contenu de cette page est réservé aux personnes connectés"
+  		redirect_to login_path
+  	end
   end
 end
